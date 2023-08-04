760e11847d3403bbdc456550c9db4fd551ff146d
Diego Ramos <die.eramos@gmail.com>
2023-08-04 10:36:20 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const lista=document.getElementById("ul");
+const contenedorDeList=document.getElementById("contenedorDeList");
+function desplegarMenu() {
+    lista.classList.toggle("desplegarMenu");
+    console.log("asd");
+ 
+}
+function sacarContenedor() {
+    contenedorDeList.classList.toggle("sacarContenedor");
+    console.log("funcion del contenedor");
+
+}
