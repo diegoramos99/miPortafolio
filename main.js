54b72f2be15fa829e06c328238d88a3f4029c3b3
die.eramos <die.eramos@gmail.com>
2024-09-12 17:32:22 -0300

    nuevo portafolio

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const lista=document.getElementById("ul");
-const contenedorDeList=document.getElementById("contenedorDeList");
-function desplegarMenu() {
-    lista.classList.toggle("desplegarMenu");
-    console.log("asd");
- 
-}
-function sacarContenedor() {
-    contenedorDeList.classList.toggle("sacarContenedor");
-    console.log("funcion del contenedor");
+let burger=document.querySelector(".bi-list")
+let menu=document.querySelector(".menu_mobile")
+let cruz=document.querySelector(".bi-x-lg")
+console.log(menu);
 
-}
+burger.addEventListener("click",()=>{
+   burger.style.display="none"
+   menu.style.display="block"
+   menu.style.top="0"
+})
+console.log(cruz);
+
+cruz.addEventListener("click",()=>{
+    burger.style.display="block"
+   
+       menu.style.top="-100%"
+})
