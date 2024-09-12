d8822d6fb0ab05446d94f64e8f41a2522e48f0bc
die.eramos <die.eramos@gmail.com>
2024-09-12 19:09:57 -0300

    nuevo portafolio

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 let burger=document.querySelector(".bi-list")
 let menu=document.querySelector(".menu_mobile")
 let cruz=document.querySelector(".bi-x-lg")
-console.log(menu);
 
+let ol=document.querySelector(".items-menu")
+let li=ol.querySelectorAll("li")
 burger.addEventListener("click",()=>{
    burger.style.display="none"
    menu.style.display="block"
    menu.style.top="0"
 })
-console.log(cruz);
 
 cruz.addEventListener("click",()=>{
     burger.style.display="block"
    
        menu.style.top="-100%"
 })
+
+li.forEach(element => {
+let a=element.querySelector("a")
+a.addEventListener('click',()=>{
+    menu.style.top="-100%"
+    burger.style.display="block"
+})
+});
+
